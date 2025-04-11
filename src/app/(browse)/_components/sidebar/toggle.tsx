31eccacc11c48549cc0b87fb481c86842c14dc69
Andrew Root <root.drew27@gmail.com>
2025-04-11 08:38:44 -0500

    Sidebarbutton now has variant='outline'

--- src/app/(browse)/_components/sidebar/toggle.tsx
+++ src/app/(browse)/_components/sidebar/toggle.tsx
@@ -14,7 +14,7 @@ const Toggle = () => {
         <>
             {collapsed && (
                 <div className="hidden md:flex w-full items-center justify-center pt-4 mb-4">
-                    <Button onClick={onExpand} className="h-auto p-2">
+                    <Button variant="outline" onClick={onExpand} className="h-auto p-2">
                         <ArrowRightFromLine className="h-4 w-4" />
                     </Button>
                 </div>
@@ -22,7 +22,7 @@ const Toggle = () => {
             {!collapsed && (
                 <div className="p-3 pl-6 mb-2 flex items-center w-full">
                     <p className="font-semibold text-primary">Hi</p>
-                    <Button onClick={onCollapse} className="h-auto p-2 ml-auto">
+                    <Button variant="outline" onClick={onCollapse} className="h-auto p-2 ml-auto">
                         <ArrowLeftFromLine className="h-4 w-4" />
                     </Button>
                 </div>
